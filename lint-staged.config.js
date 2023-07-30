a51934382d23bdf5d6505951f62b030454529b82
Rafal Doranczyk <rafal.d@masterborn.com>
2023-07-30 10:04:43 +0200

    chore: backend app

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // This will lint and format TypeScript and
-  '**/*.(ts|tsx)': filenames => [`pnpm run lint`, `pnpm run format`],
-
-  // this will Format MarkDown and JSON
-  '**/*.(md|json)': filenames => `pnpm run prettier --write ${filenames.join(' ')}`,
+  // This will lint and format TypeScript files
+  '**/*.(ts|tsx)': filenames => [`pnpm run lint`, `pnpm run format`]
 };
